cd9be90f0433f2ba0c983da3094646b3e3730ef1
stevenorduz <stevenorduz@hotmail.com>
2021-03-13 14:31:06 -0500

    COMMIT"eje3f"

--- Ejercicios/3/JS/index3.js
+++ Ejercicios/3/JS/index3.js
@@ -1,12 +1,20 @@
-function edad(obj){
+function mayormenor(obj){
 
-    var valoresAceptados = /^[.,0-9]+$/;
+    var nombress = document.getElementById("idnombre").value;
+    var edadd = document.getElementById("idedad").value;
 
-    if (obj.value.match(valoresAceptados)){
-        document.getElementById("charNum").innerHTML = '('+obj.value+')'+' NUMERO';
+    if (edadd >= 18){
+        document.getElementById("charNum").innerHTML = 'HOLA '+nombress+','+' ERES MAYOR DE EDAD';
         } else {
-            document.getElementById("charNum").innerHTML = '('+obj.value+')' +' TEXTO';
-            } if (obj.value==""){
-                document.getElementById("charNum").innerHTML = obj.value +' INGRESE UN DATO';
+            document.getElementById("charNum").innerHTML = 'HOLA '+nombress+',' +' NO ERES MAYOR DE EDAD';
+            } if (edadd==""){
+                document.getElementById("charNum").innerHTML = 'HOLA '+nombress;
             }
+                if(edadd=="0"){
+                    document.getElementById("charNum").innerHTML = '('+obj.value+')' +' NO ES UNA EDAD VALIDAD';
+                }
+                    if(obj.value==""){
+                        document.getElementById("charNum").innerHTML =' INGRESE DATOS';
+                    }
+
 }

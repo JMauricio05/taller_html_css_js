3337dbc83273ab19611d9f91e7f53ddab2e23a5e
stevenorduz <stevenorduz@hotmail.com>
2021-03-15 20:18:59 -0500

    commit "55219501"

--- 55219501/Ejercicios/6/JS/index6.js
+++ 55219501/Ejercicios/6/JS/index6.js
@@ -4,27 +4,9 @@ function buscar(obj){
     var parrafo  = document.getElementById("idparrafo").value;
     var final = document.getElementById("idfinal").value;
     
-    var keywords = [parrafo.toString()];
-    var pos = -1;
-
-    let template =`<span style="background:yellow; color:red;">${buscar}</span>`;
-
-    // uso foreach para recorrer cada elemento del array
-    keywords.forEach(function(element){
-
-    //en caso de existir se asigna la posicion
-    pos = buscar.search(element.toString());
-    
-    document.getElementById("idfinal").innerHTML = parrafo;
-    //si existe
-     if(pos!=1){
-        document.getElementById('idbuscar').style.color = "red";        
-    }  
-    });
-
-    //en caso de que no exista
-    if(pos===-1 && resultado===""){
-        document.getElementById('idbuscar').style.color = "green";
+    if(parrafo.includes(buscar)){
+        document.getElementById('idfinal').style.color = "red";
+        document.getElementById("idfinal").innerHTML = parrafo;
+  
     }
-
 }

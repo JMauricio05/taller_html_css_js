ce9ac538c0a25bd77730e504fa2b9a4b1d3e036d
stevenorduz <stevenorduz@hotmail.com>
2021-03-15 19:28:39 -0500

    commit "eje8-1"

--- 55219501/Ejercicios/8/JS/index8.js
+++ 55219501/Ejercicios/8/JS/index8.js
@@ -5,26 +5,27 @@ function parimpar(obj){
 
     var pares=[];
     var impares=[];
+    var error=[];
 
-
-    
-    for(var j=0; j<array.length;j++){
-        if(array[j]!= ","){
-            acomulador+=array[j]
-        }else{
-            if(acomulador%2==0){
-                pares.push(acomulador);
-                document.getElementById('par').style.color = "blue";
-                document.getElementById("par").innerHTML = pares+ " ES NUMERO PAR";
-        
-            }else {
-                impares.push(acomulador);
-                document.getElementById('impar').style.color = "green";
-                document.getElementById("impar").innerHTML = impares+ " ES NUMERO IMPAR";
-                
+    for(var j=0; j<array.length;j++){ 
+        if(array[j]== "."){
+        }else{  
+            if(array[j]!= ","){
+                acomulador+=array[j]
+            }else{
+                if(acomulador%2==0){
+                    pares.push(acomulador);
+                    document.getElementById('par').style.color = "blue";
+                    document.getElementById("par").innerHTML = pares+ " ES NUMERO PAR";
+            
+                }else {
+                    impares.push(acomulador);
+                    document.getElementById('impar').style.color = "green";
+                    document.getElementById("impar").innerHTML = impares+ " ES NUMERO IMPAR";
+                    
+                }
+                acomulador="";
             }
-             acomulador="";
-        }
- 
+        } 
     } 
 }

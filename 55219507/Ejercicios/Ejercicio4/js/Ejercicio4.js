ff7a8afe443a86d881c4207d23562a352b715d5e
RichiDuque <raduque@uniboyaca.edu.co>
2021-03-23 22:16:32 -0500

    Ultimos retoques

--- 55219507/Ejercicios/Ejercicio4/js/Ejercicio4.js
+++ 55219507/Ejercicios/Ejercicio4/js/Ejercicio4.js
@@ -1,16 +1,24 @@
-//let materia = [];
+
 let suma = 0;
 let prom = 0;
-function validarInputs(name,edad){
+function validarInputs(name,materia,not1,not2,not3){
     let numeros = /[0-9]/;
-    let letras = /[a-zA-Z]/;
     let nam = numeros.test(name);
-    let ed = letras.test(edad);
+    let mater = numeros.test(materia);
         if(nam){
             alert('Nombre invalido, no pueden ir numeros')
             return false;
-        }else if(ed){
-            alert('Edad invalido, no pueden ir letras')
+        }else if(mater){
+            alert('Materia invalida, no pueden ir numeros')
+            return false;
+        }else if(not1>5 || not1<0){
+            alert('nota 1 invalida, el numero debe estar entre 0-5')
+            return false;
+        }else if(not2>5 || not2<0){
+            alert('nota 2 invalida, el numero debe estar entre 0-5')
+            return false;
+        }else if(not3>5 || not3<0){
+            alert('nota 3 invalida, el numero debe estar entre 0-5')
             return false;
         }else{
             return true;
@@ -23,7 +31,7 @@ function closeModal(idModal){
 }
 
 document.getElementById('btnCalcular').addEventListener('click', function () {
-    document.getElementById('modalFormulario').classList.remove('modalClosed');
+    
     
     let nombre = document.getElementById('nombreInput').value;
     let materia = document.getElementById('materiaInput').value;
@@ -33,11 +41,16 @@ document.getElementById('btnCalcular').addEventListener('click', function () {
     let bodyResultado = document.getElementById('cadenaResult');
     suma = Number(nota1)+Number(nota2)+Number(nota3);
     prom = suma/3;
-    if(prom >= 3){
+    let validacion = validarInputs(nombre,materia,nota1,nota2,nota3);
+    if(validacion){
+        document.getElementById('modalFormulario').classList.remove('modalClosed');
+        if(prom >= 3){
         document.getElementById('divResult').classList.add('colorV');
         bodyResultado.innerHTML = 'Felicitaciones '+nombre+',<span> su nota es '+prom.toFixed(2)+'</span>,pasaste la materia '+materia;
-    }else if(prom < 3){
+        }else if(prom < 3){
         document.getElementById('divResult').classList.add('colorR');
         bodyResultado.innerHTML = 'Lo siento '+nombre+',<span> su nota es '+prom.toFixed(2)+'</span>,no pasaste la materia '+materia;
+        }
     }
+    
 });
